d7ac76fc1b9c320c84390ed267bda003f5044cd9
root <root@ESIR-ABD-BOYERE-BRAULT.istic.univ-rennes1.fr>
2017-12-04 17:48:02 +0100

    Add hdfs file

--- countWord.py
+++ countWord.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import re
 import numpy as np
+import shutil
 from random import random
 from operator import add
 
@@ -24,13 +25,18 @@ if __name__ == "__main__":
          .set("spark.executor.memory", "1g"))
 	sc = SparkContext(conf = conf)	
 
+#	shutil.rmtree('/spark-2.2.0-bin-hadoop2.7/wordsOut')
 
-	text_file = sc.textFile("file:///spark-2.2.0-bin-hadoop2.7/words.txt")
+	#text_file = sc.textFile("file:///spark-2.2.0-bin-hadoop2.7/words.txt")
+	text_file = sc.textFile("hdfs://esir-abd-boyere-brault.istic.univ-rennes1.fr:8020/user/hadoop/words.txt")
+	
+
 	counts = text_file.flatMap(parseLine) \
-             .map(lambda word: (word.split(",")[0], int(word.split(",")[1]))) \
+             .map(lambda word: (word.split(",")[0], int(word.split(",")[1])))\
              .reduceByKey(lambda a, b: a + 1)\
              .sortBy(lambda a: a[1],False) #b pour la question 1
-	#print(dir(type(counts)))
-	counts.saveAsTextFile("file:///spark-2.2.0-bin-hadoop2.7/wordsOut")
+	
+	text_file.saveAsTextFile("file:///spark-2.2.0-bin-hadoop2.7/wordsOut")
 sc.stop()
+
 
--- countWordAvg.py
+++ countWordAvg.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import re
 import numpy as np
+import shutil
 from random import random
 from operator import add
 
@@ -24,13 +25,14 @@ if __name__ == "__main__":
          .set("spark.executor.memory", "1g"))
 	sc = SparkContext(conf = conf)	
 
-
+	shutil.rmtree('/spark-2.2.0-bin-hadoop2.7/wordsOut')
 	text_file = sc.textFile("file:///spark-2.2.0-bin-hadoop2.7/words.txt")
 	map = text_file.flatMap(parseLine) \
              .map(lambda word: (word.split(",")[0], int(word.split(",")[1])))\
 	      .map(lambda nameTuple: (nameTuple[0], [ nameTuple[1] ])) \
 		.reduceByKey(lambda a, b: a+b)\
-		.mapValues(lambda x: float(sum(x))/float(len(x)))
+		.mapValues(lambda x: float(sum(x))/float(len(x)))\
+		.sortBy(lambda a: a[1],False)
 
 	
 	
